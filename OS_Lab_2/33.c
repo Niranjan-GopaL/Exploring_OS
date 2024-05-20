868d6e1b71a55f374c66c36b6a92a0d21f1c8d51
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-05-20 11:52:37 +0530

    33 done right

--- OS_Lab_2/33.c
+++ OS_Lab_2/33.c
@@ -1,16 +1,12 @@
-/*Description:
-program to send and receive data from parent to child vice versa using two way
-communication
-*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <sys/wait.h>
+
 int main(){
     int fd1[2];
     int fd2[2];
@@ -18,37 +14,44 @@ int main(){
     pipe(fd2);
 
     int id = fork();
-    //child says hello parent
-    //parent says hello child 
-    if(id==0){
-        //fd1 reader fd2 writer
-        close(fd1[1]);
-        char readbuff[20];
-        read(fd1[0],readbuff,13);
-        printf("%s",readbuff);
-        close(fd2[0]);
-        write(fd2[1],"Hello parent\n",14);
-        return 0 ; 
 
+    if(id == 0){
+        // Child process: fd1 reader, fd2 writer
+        close(fd1[1]); // Close the write end of fd1
+        close(fd2[0]); // Close the read end of fd2
 
+        char readbuff[100];
+        read(fd1[0], readbuff, 100);
+        printf("[ CHILD ] %s\n", readbuff);
 
+        const char *child_msg = "Child Proc can write here; parent can read from the other end of this buffer.";
+        write(fd2[1], child_msg, strlen(child_msg) + 1);
+
+        close(fd1[0]); // Close the read end of fd1
+        close(fd2[1]); // Close the write end of fd2
+        return 0;
+
+    } else {
+        
+        // Parent process: fd1 writer, fd2 reader
+        close(fd1[0]); // Close the read end of fd1
+        close(fd2[1]); // Close the write end of fd2
+
+        const char *parent_msg = "Parent Proc can write here; child can read from the other end of this buffer.";
+        write(fd1[1], parent_msg, strlen(parent_msg) + 1);
+
+        // Wait for the child to finish writing
+        wait(NULL); 
+
+        char readbuff[100];
+        read(fd2[0], readbuff, 100);
+        printf("[ PARENT ] %s\n", readbuff);
+
+        close(fd1[1]); // Close the write end of fd1
+        close(fd2[0]); // Close the read end of fd2
+
+        return 0;
     }
-    else{
-        //fd1 writer , fd2 reader
-        
-        close(fd1[0]);
-        write(fd1[1],"Hello Child\n",13);
-        //Need to wait for child to finish writing only then
-        //read 
-        wait(NULL);
 
-        close(fd2[1]);
-        char rbuff[20];
-        read(fd2[0],rbuff,14);
-        printf("%s",rbuff);
-        
-        
-        return 0 ; 
-    }
-    return 0 ;
+    return 0;
 }
